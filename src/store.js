6f95705f9af3d1016dba318e0c750459bd382206
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-12 17:37:59 +0100

    add store and function movies

--- src/store.js
+++ src/store.js
@@ -1,9 +1,32 @@
 import { reactive } from "vue";
+import axios from "axios";
 
-export const store = reactive({ 
+export const store = reactive({
 
-
-
-
+    searchText: "",
+    loading: false,
 
 });
+
+export function fetchMovies() {
+
+    console.log("CLICCATO CERCA BOTTONE");
+
+    axios.get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+            api_key: '320185988973d0ae03a4ed5de02ddb4d',
+            query: store.searchText,
+        }
+    })
+
+        .then((resp) => {
+            console.log(resp.data);
+        })
+
+        .catch((error) => {
+            this.ajaxError =
+                "A causa di un errore, l'operazione non è andata a buon fine";
+
+            store.loading = false;
+        });
+}
