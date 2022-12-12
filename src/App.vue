64c9d4ff9035c48c3a9331533aecb397ace6361e
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-12 17:37:45 +0100

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 
 <template>
 
+
+<AppHeader />
+
+
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+
+
 export default {
+  components: { AppHeader }
   
 }
 </script>
@@ -12,6 +20,9 @@ export default {
 <style lang="scss">
 @use "./styles/general.scss";
 
+body{
+  background-color: rgba(0, 0, 0, 0.815);
+}
 
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <header>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search" v-model="store.searchText">
+                    <button class="btn btn-outline-success" @click.prevent="fetchMovies">Cerca</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+</template>
+
+
+<script>
+import {store, fetchMovies} from '../store.js';
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        fetchMovies,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
