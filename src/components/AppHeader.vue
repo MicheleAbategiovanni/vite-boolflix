4c81bad1d2ed26ebb9d62b4b5566b15731608160
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-15 08:46:48 +0100

    fix color hamburger menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,7 +11,7 @@
                     </a>
                 </div>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
